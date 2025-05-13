bf4298397a78cc6b1c95182e6c5734a1c76454c0
Roniel Sagadal <notkinshen@gmail.com>
2025-05-13 21:30:45 +0800

    Add files via upload

---
+++ scripts/js/fireConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+ apiKey: "AIzaSyBKpLbfYwpVEZs_D30B9GnyPiVvyZ-9Dwc",
+  authDomain: "sagadal-activity.firebaseapp.com",
+  databaseURL: "https://sagadal-activity-default-rtdb.firebaseio.com",
+  projectId: "sagadal-activity",
+  storageBucket: "sagadal-activity.firebasestorage.app",
+  messagingSenderId: "1009443340041",
+  appId: "1:1009443340041:web:c666f5e57d9c9b8d8f1dd8",
+  measurementId: "G-1YMJE7HKB0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
